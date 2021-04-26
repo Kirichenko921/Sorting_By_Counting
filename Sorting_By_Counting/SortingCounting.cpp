19f57206feb40274c7265af7e3c5aee468622232
Kirichenko Alexander <050780@inbox.ru>
2021-04-26 19:45:56 +0300

    оставил только сортировку

--- Sorting_By_Counting/SortingCounting.cpp
+++ Sorting_By_Counting/SortingCounting.cpp
@@ -1,64 +1,7 @@
-﻿#include <string>
-#include<fstream>
-#include<iostream>
-#include<cstdlib>
-#include <ctime>
-#include <chrono> // для получения времени
+﻿#include<iostream>
+
 using namespace std;
 
-void write_arr(const string& filename, const int* arr, const int n)
-{
-	fstream fs;
-	fs.open(filename, fstream::out);
-	if (fs.is_open())// проверяем что файл успешно открыт
-	{
-		fs << n << '\n';//записываем размер массива
-		for (int i = 0; i < n; i++)
-			fs << arr[i] << ' ';//записываем значения через пробел
-		fs << '\n';
-
-		fs.close();//закрываем файл
-	}
-}
-
-void read_arr(const string& filename, int* arr, int n)
-{
-	fstream fs;
-
-	fs.open(filename, fstream::in);
-	if (fs.is_open())// проверяем что файл успешно открыт
-	{
-		fs >> n; // читаем размер массива
-		arr = new int[n];
-		for (int i = 0; i < n; ++i)
-			fs >> arr[i];// читаем из файла разделительные символы - пробел и перенос строки
-
-		fs.close();//закрываем файл
-	}
-}
-void swap(int* a, int* b) // функция для смены местами двух значений
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-int partition(int* arr, int low, int high)
-{
-	int pivot = arr[high]; // опорный элемент
-	int i = (low - 1); // индекс наименьшего элемента
-	for (int j = low; j <= high - 1; j++)
-	{
-		// если текущий элемент меньше или равен опорнуму
-		if (arr[j] <= pivot)
-		{
-			i++; // увеличиваем индекс минимального элемента
-			swap(&arr[i], &arr[j]);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
-}
 int getMax(int *arr, int n) // фунция для нахождения максимального элемента
 {
 	int max = arr[0];
@@ -87,18 +30,13 @@ void countSort(int* arr, int n)
 		count[arr[i]]++; // подсчитываем частоту повторения элементов
 		
 	}
-	for (int i = 0; i <= max; ++i)
-	{
-		cout << count[i] << " ";
-	}
-	cout << endl;
+	
 	
 	for (int i = 0, j = 0; i <= max;i++)
 	{
 		while (count[i]--)
 			output[j++] = i;
 	}
-	cout << endl;
 	
 
 	for (int i = 0; i < n; i++)
